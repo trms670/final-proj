71397dffbd920e04fc9553ad333c05c9eddb4f48
Tyler <tyler.tan@yale.edu>
2022-12-10 21:17:03 -0500

    dropdown

--- app.py
+++ app.py
@@ -1,22 +1,22 @@
-import os
+# import os
 from flask import Flask, flash, redirect, render_template, request, session
-from flask_session.__init__ import Session
-from tempfile import mkdtemp
+# from flask_session.__init__ import Session
+# from tempfile import mkdtemp
 
 app = Flask(__name__)
 
 # Configure session to use filesystem (instead of signed cookies)
-app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_TYPE"] = "filesystem"
-Session(app)
+# app.config["SESSION_PERMANENT"] = False
+# app.config["SESSION_TYPE"] = "filesystem"
+# Session(app)
 
-@app.after_request
-def after_request(response):
-    """Ensure responses aren't cached"""
-    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
-    response.headers["Expires"] = 0
-    response.headers["Pragma"] = "no-cache"
-    return response
+# @app.after_request
+# def after_request(response):
+#     """Ensure responses aren't cached"""
+#     response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+#     response.headers["Expires"] = 0
+#     response.headers["Pragma"] = "no-cache"
+#     return response
 
 
 @app.route("/", methods=["GET", "POST"])
