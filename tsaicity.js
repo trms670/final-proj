ffdf42f02eea26f0cef063c757ed05af44fce023
Tyler <tyler.tan@yale.edu>
2022-12-11 19:04:29 -0500

    table

--- tsaicity.js
+++ tsaicity.js
@@ -1,10 +1,13 @@
 var post= document.getElementById("submit");
 post.addEventListener("click", function(){
     var commentBoxValue= document.getElementById("comment-box").value;
- 
-    var li = document.createElement("li");
-    var text = document.createTextNode(commentBoxValue);
-    li.appendChild(text);
-    document.getElementById("unordered").appendChild(li);
+    let table = document.getElementById("reviews");
+    let row = table.insertRow();
+    let c1 = row.insertCell(0);
+    c1.innerText = commentBoxValue;
+    // var li = document.createElement("li");
+    // var text = document.createTextNode(commentBoxValue);
+    // li.appendChild(text);
+    // document.getElementById("unordered").appendChild(li);
  
 });
