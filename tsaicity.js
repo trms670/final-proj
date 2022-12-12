f4875c18cd66cb3cc09d89d7cfb944622c056767
Tyler <tyler.tan@yale.edu>
2022-12-11 22:12:24 -0500

    error

--- tsaicity.js
+++ tsaicity.js
@@ -1,9 +1,16 @@
 var post= document.getElementById("submit");
 post.addEventListener("click", function(){
     var commentBoxValue= document.getElementById("comment-box").value;
-    let table = document.getElementById("reviews");
-    let row = table.insertRow();
-    let c1 = row.insertCell(0);
-    c1.innerText = commentBoxValue;
-    document.getElementById("comment-box").value = "";
+    var warning = document.getElementById("warning");
+    if (commentBoxValue == "") {
+        warning.removeAttribute("hidden");
+    }
+    else {
+        warning.setAttribute("hidden", true);
+        let table = document.getElementById("reviews");
+        let row = table.insertRow();
+        let c1 = row.insertCell(0);
+        c1.innerText = commentBoxValue;
+        document.getElementById("comment-box").value = "";
+    }
 });
